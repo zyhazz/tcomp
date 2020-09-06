e0f041bfb60253999a2bdb552c0bbe3e1be9b8f7
Diego Carvalho <zyhazz@gmail.com>
2020-09-06 11:23:51 -0300

    inicial

---
+++ b/dfa.js
@@ -0,0 +1,13 @@
+class DFA{
+  constructor(estados, alfabeto, transicoes, inicial, finais){
+    this.estados = estados;
+    this.alfabeto = alfabeto;
+    this.transicoes = transicoes;
+    this.inicial = inicial;
+    this.finais = finais;
+  }
+  
+  verificar(string){
+
+  }
+}
